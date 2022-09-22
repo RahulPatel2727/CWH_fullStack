5a6dc94610db9491efa34d340ef29d7dee71e8ee
RahulPatel2727 <rahul23131420@gmail.comm>
2022-09-22 10:54:01 +0530

    initial commit

---
+++ javaScript/2_variables_Operators/js.js
@@ -0,0 +1,6 @@
+var x = 40;
+var y = "rahul";
+var c = 'r';
+console.log(c);
+var a=2,b=9;
+console.log("result "+b%2);
---
+++ javaScript/3_Strings_StringMethods/js.js
@@ -0,0 +1,4 @@
+console.log("hello");
+var name = 'rahul';
+var email = 'rahul.patel19042000@gmail.com'
+console.log(name +' '+ email);
